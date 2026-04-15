a0be668314d8f17d64261e396a30e42c7888b7e6
Stefan Trenkwalder <stefan@trenkwalder.phd>
2026-04-15 22:04:54 +0200

    Add more sanity checks for different types of weights

--- src/shipping_costs/calculator.py
+++ src/shipping_costs/calculator.py
@@ -1,6 +1,10 @@
+from typing import NewType
+
+WeightInKg = NewType("WeightInKg", float)
+CostsInEuro = NewType("CostsInEuro", float)
 
 BASE_SHIPPING_RATE = 5.0
-LOW_TIER_COSTS = 1.0
+LOW_TIER_COSTS_PER_KG = 1.0
 
-def calculate_shipping_costs(weight: float) -> float:
-    return BASE_SHIPPING_RATE + weight*LOW_TIER_COSTS
+def calculate_shipping_costs(weight: WeightInKg) -> CostsInEuro:
+    return BASE_SHIPPING_RATE + CostsInEuro(weight)*LOW_TIER_COSTS_PER_KG
--- tests/features/test_shipping_costs.py
+++ tests/features/test_shipping_costs.py
@@ -1,11 +1,12 @@
 import pytest
 
-from shipping_costs.calculator import calculate_shipping_costs, BASE_SHIPPING_RATE, LOW_TIER_COSTS
+from shipping_costs.calculator import calculate_shipping_costs, BASE_SHIPPING_RATE, LOW_TIER_COSTS_PER_KG, WeightInKg
 
 
 def test_shipping_of_an_empty_order_costs_the_base_rate() -> None:
     assert calculate_shipping_costs(weight=0) == BASE_SHIPPING_RATE
 
 
-def test_when_shipping_less_than_five_kg_it_costs_increased_rate() -> None:
-    assert calculate_shipping_costs(weight=3) == BASE_SHIPPING_RATE + 3.0*LOW_TIER_COSTS
+@pytest.mark.parametrize("weight", [1, 3, 4.99])
+def test_shipping_costs_for_low_weight_tiers(weight: WeightInKg) -> None:
+    assert calculate_shipping_costs(weight=weight) == BASE_SHIPPING_RATE + float(weight) * LOW_TIER_COSTS_PER_KG
